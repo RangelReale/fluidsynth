e2bce900e9e65f115c3b827cd84493343e640848
Rangel Reale <rangelspam@gmail.com>
2018-11-28 15:55:04 -0200

    Public memory functions

---
+++ include/fluidsynth/memory.h
@@ -0,0 +1,43 @@
+/* FluidSynth - A Software Synthesizer
+ *
+ * Copyright (C) 2003  Peter Hanappe and others.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA
+ */
+
+#ifndef _FLUIDSYNTH_MEMORY_H
+#define _FLUIDSYNTH_MEMORY_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @file memory.h
+ * @brief Memory interface
+ *
+ * Memory functions.
+ */
+
+FLUIDSYNTH_API void* fluid_malloc(int size);
+FLUIDSYNTH_API void *fluid_remalloc(void* ptr, int size);
+FLUIDSYNTH_API void *fluid_free(void *ptr);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _FLUIDSYNTH_LOG_H */
